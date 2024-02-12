47c1d37b1cb32e890956f47b89e7612315330f25
Jay Gheewala <jaygheewala157@gmail.com>
2024-02-12 11:02:27 +0530

    CRUD Operations On Employee Module

--- frmEmployee.vb
+++ frmEmployee.vb
@@ -6,10 +6,10 @@ Public Class frmEmployee
     Dim conn As New OleDbConnection
     Dim cmd As New OleDbCommand
     Dim ds As New DataSet
+    Dim adp As New OleDbDataAdapter
 
     Private Sub frmEmployee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        frmOverView.Hide()
         conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\Medical Store Management System\My Project\Medical Store Management System.accdb"
         conn.Open()
         showdata()
@@ -18,17 +18,92 @@ Public Class frmEmployee
 
     Sub showdata()
 
-        If ds.Tables.Count > 0 Then
-            ds.Clear()
-        End If
-        Dim adp As New OleDbDataAdapter("SELECT * FROM Employee", conn)
+        ds.Clear()
+        adp = New OleDbDataAdapter("SELECT * FROM Employee", conn)
         adp.Fill(ds, "Employee")
         dgvEmployee.DataSource = ds.Tables("Employee")
 
     End Sub
 
-    Private Sub frmEmployee_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+    Private Sub frmEmployee_Closing(sender As Object, e As CancelEventArgs) Handles MyBase.Closing
         conn.Close()
     End Sub
 
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        Try
+
+            Dim query As String = "INSERT INTO Employee(Emp_Name,Emp_UserName,Emp_password,Emp_Salary,Emp_Type) VALUES('" & txtName.Text & "','" & txtUsername.Text & "','" & txtPassword.Text & "'," & txtSalary.Text & ",'" & txtType.Text & "')"
+            cmd = New OleDbCommand(query, conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Employee data inserted successfully!")
+            showdata()
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error inserting employee data: " & ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+
+        Try
+
+            Dim query As String = "UPDATE Employee SET Emp_Name='" & txtName.Text & "', Emp_UserName='" & txtUsername.Text & "', Emp_password='" & txtPassword.Text & "', Emp_Salary=" & txtSalary.Text & ", Emp_Type='" & txtType.Text & "' WHERE Emp_Id=" & txtId.Text & ""
+            cmd = New OleDbCommand(query, conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Employee data Updated successfully!")
+            showdata()
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error Updating employee data: " & ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        Try
+
+            Dim query As String = "DELETE FROM Employee  WHERE Emp_Id=" & txtId.Text & ""
+            cmd = New OleDbCommand(query, conn)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Employee data Deleted successfully!")
+            showdata()
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error Deleting employee data: " & ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        Try
+
+            Dim query As String = "SELECT * FROM Employee WHERE Emp_Id=" & txtId.Text & ""
+            ds.Clear()
+            adp = New OleDbDataAdapter(query, conn)
+            adp.Fill(ds, "Employee")
+            dgvEmployee.DataSource = ds.Tables("Employee")
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error Displaying employee data: " & ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnShowAll_Click(sender As Object, e As EventArgs) Handles btnShowAll.Click
+
+        showdata()
+
+    End Sub
 End Class
--- frmOverView.vb
+++ frmOverView.vb
@@ -1,3 +1,7 @@
 ﻿Public Class frmOverView
+    Private Sub frmOverView_Load(sender As Object, e As EventArgs) Handles Me.Load
 
+
+
+    End Sub
 End Class
